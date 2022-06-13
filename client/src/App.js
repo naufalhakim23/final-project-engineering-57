4586c100d1a8492a40ee7fbff43e8fd796b4c231
naufalhakim <hakimnaufal212@gmail.com>
2022-06-13 21:18:13 +0700

    Feat: Adding Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { ChakraProvider, Box, theme } from '@chakra-ui/react';
+import { ChakraProvider, theme } from '@chakra-ui/react';
 // import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        {/* <ColorModeSwitcher justifySelf="flex-end" /> */}
-        <LandingPage />
-      </Box>
+      {/* <ColorModeSwitcher justifySelf="flex-end" /> */}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/sign-in" element={<SignIn />} />
+      </Routes>
     </ChakraProvider>
   );
 }
