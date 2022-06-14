cffec3125d2e572e398da167837f3d6770267667
ElricoAf <elricoafd@outlook.com>
2022-06-14 10:29:50 +0700

    Revert "halaman login"
    This reverts commit 11ef4d743acc9320f6429ada93f7b47c58166b7b.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   ChakraProvider,
-  IconButton,
   Box,
   Text,
   Link,
@@ -9,87 +8,35 @@ import {
   Code,
   Grid,
   theme,
-  Flex,
-  Heading,
-  FormControl,
-  FormLabel,
-  Input,
-  Stack,
-  Checkbox,
-  Button,
-  ColorModeProvider,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-
+import { Logo } from './Logo';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <ColorModeProvider> 
-      <Box fontWeight='bold' fontSize='48px' mt={10} align="center" justifyContent="center">IMPIANMU</Box>
-      <Login />
-      </ColorModeProvider>
-        
-   
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <Logo h="40vmin" pointerEvents="none" />
+            <Text>
+              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+            </Text>
+            <Link
+              color="teal.500"
+              href="https://chakra-ui.com"
+              fontSize="2xl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn Chakra
+            </Link>
+          </VStack>
+        </Grid>
+      </Box>
     </ChakraProvider>
   );
 }
 
-const Login = () => {
-  return (
-    <Flex minHeight ='75vh'width='full' align="center" justifyContent='center'>
-      <Box
-      borderWidth={1}
-      px={4}
-      width='full'
-      maxWidth='500px'
-      borderRadius={4}
-      boxShadow='lg'>
-        <ColorModeSwitcher />
-        <Box p={3}>
-        <LoginHeader />
-        <LoginForm />
-      </Box>
-      </Box>
-    </Flex>
-  )
-}
-
-const LoginHeader = () => {
-  return (
-    <Box textAlign='center'>
-      <Heading>Masuk Ke Akunmu</Heading>
-      <Text>atau <Link>Daftar?</Link> </Text>
-    </Box>
-  )
-}
-
-const LoginForm = () => {
-  return (
-    <Box my={8} >
-      <form>
-        <FormControl>
-          <FormLabel>Email</FormLabel>
-          <Input type="email" placeholder="Masukkan email" />
-        </FormControl>
-        <FormControl mt={4}>
-          <FormLabel>Password</FormLabel>
-          <Input type="Password" placeholder="Masukkan rassword" />
-        </FormControl>
-
-
-        <Stack isInline justifyContent='space-between'mt={4}>
-          <Box>
-            <Checkbox>Ingat Saya</Checkbox>
-          </Box>
-          <Box>
-            <Link>Lupa Password?</Link>
-          </Box>
-        </Stack>
-
-        <Button width='full' mt ={4}>Masuk</Button>
-      </form>
-    </Box>
-  )
-}
 export default App;
