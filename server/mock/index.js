355b4cd3bd401cd694db77dbd232e4b6b17afc21
naufalhakim <hakimnaufal212@gmail.com>
2022-06-20 21:28:42 +0700

    Add: user, test, explanation router in index.js

--- server/mock/index.js
+++ server/mock/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const jsonServer = require("json-server");
 const app = express();
 
 const AuthRouter = require("./src/routers/AuthRouter");
-// const UserRouter = require("./src/routers/UserRouter");
+const UserRouter = require("./src/routers/UserRouter");
+const TestRouter = require("./src/routers/TestRouter");
+const ExplanationRouter = require("./src/routers/ExplanationRouter");
 
 //middleware
 app.use(express.json());
@@ -11,7 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 
 //Router
 app.use("/api/auth", AuthRouter);
-// app.use("/api/user", UserRouter);
+app.use("/api/user", UserRouter);
+app.use("/api/test", TestRouter);
+app.use("/api/explanation", ExplanationRouter);
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
