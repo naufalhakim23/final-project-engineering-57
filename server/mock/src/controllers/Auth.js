352bd6d030aba57decdf38f5920c2c49ef8a61f1
rpo <hakimnaufal212@gmail.com>
2022-06-27 14:52:20 +0700

    Feat: Changing mysql database to postgresql heroku using pool.query

--- server/mock/src/controllers/Auth.js
+++ server/mock/src/controllers/Auth.js
@@ -1,189 +1,142 @@
 const uuid = require("uuid").v4;
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const db = require("../db/db");
+const pool = require("../db/db");
 class AuthController {
   // //Get User test (don't implement this because it's sensitive data)
-  // static getUser(req, res) {
-  //   res.status(200).json({
-  //     message: "User retrieved successfully",
-  //     authUsers,
-  //   });
-  // }
+  static getUser(req, res) {
+    try {
+      pool.query("SELECT * FROM impianmu.users", (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({
+            error: err,
+          });
+        } else {
+          res.status(200).json(result.rows);
+        }
+      });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
   //Register User
   static register(req, res) {
-    db.query(
-      "SELECT * FROM userdb WHERE email = ?",
-      [req.body.email],
-      (err, results) => {
-        if (err) {
-          res.status(500).json({
-            message: "Error registering user",
-            error: err,
-          });
-        } else if (results.length > 0) {
-          res.status(409).json({
-            message: "User already exists",
-          });
-        } else {
-          const { email, password } = req.body;
-          const id = uuid();
-          const hash = bcrypt.hashSync(password, 10);
-          db.query(
-            "INSERT INTO userdb (id, first_name, last_name, email, password, role, created_at) VALUES (?, ?, ?, ?, ?, ?, ?)",
-            [
-              id,
-              req.body.first_name,
-              req.body.last_name,
-              email,
-              hash,
-              "user",
-              new Date(),
-            ],
-            (err, results) => {
-              if (err) {
-                res.status(500).json({
-                  message: "Error registering user",
-                  error: err,
-                });
-              } else {
-                res.status(201).json({
-                  message: "User registered successfully",
-                  authUser: {
-                    id,
-                    first_name: req.body.first_name,
-                    last_name: req.body.last_name,
-                    email,
-                    password: hash,
-                    role: "user",
-                    created_at: new Date(),
-                  },
-                });
-              }
-            }
-          );
-        }
-      }
-    );
-    // const { email, password, firstName, lastName } = req.body;
-    // if (email === null) {
-    //   res.status(400).json({
-    //     message: "Email is required",
-    //   });
-    // } else if (password === null) {
-    //   res.status(400).json({
-    //     message: "Password is required",
-    //   });
-    // } else if (firstName === null) {
-    //   res.status(400).json({
-    //     message: "First name is required",
-    //   });
-    // } else if (lastName === null) {
-    //   res.status(400).json({
-    //     message: "Last name is required",
-    //   });
-    // } else {
-    //   const user = authUsers.find((user) => user.email === email);
-    //   if (user) {
-    //     res.status(400).json({
-    //       message: "Email already exists",
-    //     });
-    //   } else {
-    //     const newUser = {
-    //       id: uuid(),
-    //       email,
-    //       password: bcrypt.hashSync(password, 10),
-    //       firstName,
-    //       lastName,
-    //       role: "user",
-    //     };
-    //     authUsers.push(newUser);
-    //     try {
-    //       res.status(201).json({
-    //         message: "User created successfully",
-    //       });
-    //     } catch (error) {
-    //       res.status(500).json({ message: error.message });
-    //     }
-    //   }
-    // }
-  }
-  //Login User
-  static login(req, res) {
-    db.query(
-      "SELECT * FROM userdb WHERE email = ?",
-      [req.body.email],
-      (err, results) => {
-        if (err) {
-          res.status(500).json({
-            message: "Error logging in user",
-            error: err,
-          });
-        } else if (results.length === 0) {
-          res.status(401).json({
-            message: "User does not exist",
-          });
-        } else {
-          const userdb = results[0];
-          if (bcrypt.compareSync(req.body.password, userdb.password)) {
-            const token = jwt.sign(
-              {
-                id: userdb.id,
-                email: userdb.email,
-                firstName: userdb.first_name,
-                lastName: userdb.last_name,
-              },
-              "secret",
-              {
-                expiresIn: "1h",
-              }
-            );
-            // console.log(token);
-            res.status(200).json({
-              message: "User logged in successfully",
-              token,
+    try {
+      const { username, password } = req.body;
+      const hash = bcrypt.hashSync(password);
+      const id = uuid();
+      pool.query(
+        //Check email if it's already registered
+        "SELECT * FROM impianmu.users WHERE email = $1",
+        [req.body.email],
+        (err, result) => {
+          if (err) {
+            console.log(err);
+            res.status(500).json({
+              error: err,
+            });
+          } else if (result.rows.length > 0) {
+            res.status(400).json({
+              message: "Email already registered",
             });
           } else {
-            res.status(401).json({
-              message: "Incorrect password",
-            });
+            pool.query(
+              //Insert new user
+              "INSERT INTO impianmu.users (id, first_name, last_name, email, password, role, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+              [
+                id,
+                req.body.first_name,
+                req.body.last_name,
+                req.body.email,
+                hash,
+                "user",
+                new Date(),
+                new Date(),
+              ],
+              (err, result) => {
+                if (err) {
+                  console.log(err);
+                  res.status(500).json({
+                    error: err,
+                  });
+                } else {
+                  res.status(201).json({
+                    message: "User created",
+                  });
+                }
+              }
+            );
           }
         }
-      }
-    );
-    // const { email, password } = req.body;
-    // const authUser = authUsers.find((user) => user.email === email);
-    // if (!authUser) {
-    //   return res.status(401).json({ message: "Invalid email or password" });
-    // } else if (!bcrypt.compareSync(password, authUser.password)) {
-    //   return res.status(401).json({ message: "Invalid email or password" });
-    // } else {
-    //   try {
-    //     const token = jwt.sign({ id: authUser.id }, "secret");
-    //     res.status(200).json({
-    //       message: "User logged in successfully",
-    //       token,
-    //     });
-    //   } catch (error) {
-    //     res.status(500).json({ message: error.message });
-    //   }
-    // }
+      );
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
   }
-  //Checking if user is logged in
+  // //Login User
+  static login(req, res) {
+    try {
+      pool.query(
+        "SELECT * FROM impianmu.users WHERE email = $1",
+        [req.body.email],
+        (err, result) => {
+          if (err) {
+            console.log(err);
+            res.status(500).json({
+              error: err,
+            });
+          } else if (result.rows.length === 0) {
+            res.status(400).json({
+              message: "User does not exist",
+            });
+          } else {
+            const user = result.rows[0];
+            if (bcrypt.compareSync(req.body.password, user.password)) {
+              const token = jwt.sign(
+                {
+                  id: user.id,
+                  email: user.email,
+                  firstName: user.first_name,
+                  lastName: user.last_name,
+                  role: user.role,
+                },
+                "secret",
+                {
+                  expiresIn: "1h",
+                }
+              );
+              res.status(200).json({
+                message: "User logged in successfully",
+                token: token,
+              });
+            } else {
+              res.status(400).json({
+                message: "Incorrect password",
+              });
+            }
+          }
+        }
+      );
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+  //check if user is logged in
   static check(req, res) {
     if (req.headers.authorization) {
       const token = req.headers.authorization.split(" ")[1];
-      try {
-        // const decoded = jwt.verify(token, "secret");
-        res.status(200).json({
-          message: "User is logged in",
-          // decoded,
-        });
-      } catch (error) {
-        res.status(401).json({ message: "Invalid token" });
-      }
-    } else {
-      res.status(401).json({ message: "No token provided" });
+      jwt.verify(token, "secret", (err, decoded) => {
+        if (err) {
+          res.status(401).json({
+            message: "Invalid token",
+          });
+        } else {
+          res.status(200).json({
+            message: "User is logged in",
+          });
+        }
+      });
     }
   }
 }
--- server/mock/src/controllers/Test.js
+++ server/mock/src/controllers/Test.js
@@ -1,16 +1,15 @@
-const db = require("../db/db");
-
+const pool = require("../db/db");
 class TestController {
   //Get All Test
   static getAllTest(req, res) {
-    db.query("SELECT * FROM question", (err, results) => {
+    pool.query("SELECT * FROM impianmu.questions", (err, result) => {
       if (err) {
+        console.log(err);
         res.status(500).json({
-          message: "Error getting all test",
           error: err,
         });
       } else {
-        res.status(200).json(results);
+        res.status(200).json(result.rows);
       }
     });
   }
@@ -26,45 +25,60 @@ class TestController {
   }
   //Create Test Question and add to the list
   static createTest(req, res) {
-    const newTest = {
-      id: question.length + 1,
-      question: req.body.question,
-      answerFor: req.body.answerFor,
-      options: req.body.options,
-    };
-    question.push(newTest);
-    try {
-      res.status(201).json(newTest);
-    } catch (error) {
-      res.status(400).json({ message: "Bad Request" });
-    }
+    pool.query(
+      "INSERT INTO impianmu.questions (question, answerFor, options) VALUES ($1, $2, $3)",
+      [req.body.question, req.body.answerFor, req.body.options],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({
+            error: err,
+          });
+        } else {
+          res.status(201).json({
+            message: "Test created",
+          });
+        }
+      }
+    );
   }
   //Update Test Question
   static updateTest(req, res) {
-    const { id } = req.params;
-    const test = question.find((test) => test.id === parseInt(id));
-    if (!test) {
-      res.status(404).json({ message: "Test not found" });
-    }
-    const updateTest = {
-      id: test.id,
-      question: req.body.question,
-      answerFor: req.body.answerFor,
-      options: req.body.options,
-    };
-    question.splice(question.indexOf(test), 1, updateTest);
-    res.status(200).json(updateTest);
+    pool.query(
+      "UPDATE impianmu.questions SET question = $1, answerFor = $2, options = $3 WHERE id = $4",
+      [req.body.question, req.body.answerFor, req.body.options, req.params.id],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({
+            error: err,
+          });
+        } else {
+          res.status(200).json({
+            message: "Test updated",
+          });
+        }
+      }
+    );
   }
   //Delete Test Question
   static deleteTest(req, res) {
-    const { id } = req.params;
-    const test = question.find((test) => test.id === parseInt(id));
-    if (!test) {
-      res.status(404).json({ message: "Test not found" });
-    } else {
-      question.splice(question.indexOf(test), 1);
-      res.status(200).json({ message: "Test deleted" });
-    }
+    pool.query(
+      "DELETE FROM impianmu.questions WHERE id = $1",
+      [req.params.id],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({
+            error: err,
+          });
+        } else {
+          res.status(200).json({
+            message: "Test deleted",
+          });
+        }
+      }
+    );
   }
 }
 module.exports = TestController;
