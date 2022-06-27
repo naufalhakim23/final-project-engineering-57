30de48e3a4eb7299345e93a1fff2e6529cc784b9
naufalhakim <hakimnaufal212@gmail.com>
2022-06-27 14:56:06 +0700

    Chore: Connecting to test results

--- client/src/pages/TestMenu/ResultTest.jsx
+++ client/src/pages/TestMenu/ResultTest.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import HeaderDashboard from '../../components/DashboardComponents/HeaderDashboard.tsx';
 import Footer from '../../components/Footer.tsx';
-import { Box, Flex, Heading } from '@chakra-ui/react';
+import { Box, Flex, Heading, Button } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 const ResultTest = () => {
     return (
         <>
@@ -23,10 +24,32 @@ const ResultTest = () => {
                         fontWeight={'600'}
                         color={'black'}
                         margin={5}
-
                     >
-                        Hasil Tes RIASEC
+                        RIASEC Test Result
                     </Heading>
+                    <Box
+                        alignItems={'right'}
+                        justifyContent={'right'}
+                        display={'flex'}
+                        marginTop={'1rem'}
+                        width={'100%'}
+                    >
+                        <Link to={'/home'}>
+                            <Button
+                                background={'#00D56373'}
+                                color={'#FFF'}
+                                fontSize={'1rem'}
+                                fontWeight={'600'}
+                                borderRadius={'10px'}
+                                padding={'1rem'}
+                                margin={'1rem'}
+                                _hover={{ background: '#FFF', color: '#00D56373' }}
+                                _active={{ background: '#00D56373' }}
+                            >
+                                Kembali
+                            </Button>
+                        </Link>
+                    </Box>
                 </Flex>
             </Box >
             <Footer />
